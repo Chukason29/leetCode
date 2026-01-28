0f4ac4f692fea802ed5022b28c81be3467d56c13
chukwuebuka alaegbu <chukwuebukaalaegbu@gmail.com>
2026-01-28 12:02:57 +0100

    two Sums

---
+++ twoSums.py
@@ -0,0 +1,11 @@
+"""
+1️⃣ Two Sum
+
+Given an array of integers called "nums" and an integer "target", return the indices of the two numbers such that they add up to target.
+
+📌 Each input has exactly one solution
+📌 You may not use the same element twice
+"""
+
+nums = [2, 9, 7, 4, 3]
+target =  12
